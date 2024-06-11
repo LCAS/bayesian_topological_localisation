03f900c9e3c572ae8b2892175385ba643e87abcc
Nikolaus Wagner <nikolaus.wagner@protonmail.com>
2024-06-11 12:32:20 +0200

    feat: Add launch file for entire localisation

---
+++ src/bayesian_topological_localisation/launch/bayesian_localisation_launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+  return LaunchDescription([
+    Node(
+      package="topological_navigation",
+      executable="map_manager2.py",
+      arguments=["/home/ros/ros_ws/src/bayesian_topological_localisation/config/riseholme_tagged.tmap2.yaml"],
+      name="topological_map_manager"
+
+    ),
+    Node(
+      package="topological_navigation",
+      executable="topological_transform_publisher.py",
+      name="topological_transform_publisher"
+    ),
+    Node(
+      package="topological_navigation",
+      executable="topomap_marker2.py",
+      name="topological_map_visualiser"
+    ),
+    Node(
+      package="bayesian_topological_localisation",
+      executable="tpf_car_handler.py",
+      name="tpf_car_handler"
+    ),
+    Node(
+      package="bayesian_topological_localisation",
+      executable="localisation_node.py",
+      name="bayes_topological_localisation"
+    )
+  ])
